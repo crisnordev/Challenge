e13fe722a30e7a3b63df064604002f2f21c9d312
crisnordev <crisnordev@gmail.com>
2022-09-14 23:01:13 -0300

    Creating ViewModels/CourseViewModels/GetCourseByIdViewModel.cs, and refactoring Error.cshtml

---
+++ ViewModels/CourseViewModels/GetCourseByIdViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Challenge.ViewModels.CourseViewModels;
+
+public class GetCourseByIdViewModel
+{
+    public GetCourseByIdViewModel() { }
+
+    public Guid CourseId { get; set; }
+    
+    [Display(Name = "Course")] public string CourseTitle { get; set; }
+
+    [Display(Name = "Tag")] public string Tag { get; set; }
+
+    [Display(Name = "Summary")] public string Summary { get; set; } = string.Empty;
+
+    [Display(Name = "Duration")] public int Duration { get; set; }
+    
+    [Display(Name = "Modules")] public Dictionary<Guid, string> CourseItems { get; set; } = null!;
+
+    public static implicit operator GetCourseByIdViewModel(Course course)
+    {
+        var courseView = new GetCourseByIdViewModel
+        {
+            CourseId = course.CourseId,
+            CourseTitle = course.CourseTitle,
+            Tag = course.Tag,
+            Summary = course.Summary,
+            Duration = course.Duration
+        };
+        foreach (var courseItem in course.CourseItems)
+        {
+            courseView.CourseItems.Add(courseItem.CourseItemId, courseItem.CourseItemTitle);
+        }
+
+        return courseView;
+    }
+}
